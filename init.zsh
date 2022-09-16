e2ff6088affe5e41e646161ea7b595f4044e94a1
Joke de Buhr <joke@xckk.de>
2022-09-16 12:55:46 +0200

    feat: yq completions
    Signed-off-by: Joke de Buhr <joke@xckk.de>

---
+++ init.zsh
@@ -0,0 +1,8 @@
+(( ${+commands[yq]} || ${+commands[asdf]} && ${+functions[_direnv_hook]} )) && () {
+
+  local command=${commands[yq]:-"$(${commands[asdf]} which kn 2> /dev/null)"}
+  [[ -z $command ]] && return 1
+
+  local compfile=$1/functions/_yq
+  [[ ! -e $compfile || $compfile -ot $command ]] && $command shell-completion zsh >| $compfile
+} ${0:h}
